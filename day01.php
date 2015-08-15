3e2cfbc74ae35f419071a863be1f07dd7a90eaa6
Khairul Azam <skyview059@gmail.com>
2015-08-16 03:20:35 +0600

    Update day01.php

--- day01.php
+++ day01.php
@@ -1,1 +1,41 @@
 <?php
+
+class Student{
+ 
+ 	function result($mark = 0){
+		
+		switch ($mark) {
+			case ($mark <= 59):
+				$result = 'You are fail!. Your Mark is '. $mark;
+				break;
+			case ($mark <= 69):
+				$result = 'You Just Passed. Your Mark is '. $mark;
+				break;
+			case ($mark <= 79):
+				$result = 'You got "D" grade. Your Mark is '. $mark;
+				break;
+			
+			case ($mark <= 89):
+				$result = 'You got "C" grade. Your Mark is '. $mark;
+				break;
+				
+			case ($mark <= 99):
+				$result = 'You got "B" grade. Your Mark is '. $mark;
+				break;
+				
+			case ($mark == 100):
+				$result = 'You got "A" grade. Your Mark is '. $mark;
+				break;								
+			default :
+				$result = "No Case Match";
+				break;
+		}
+		$color = ($mark >= 60 ) ? "green" : "red";
+		
+		return "<b style='color:$color; font-family:arial; font-size:30pt;'>" . $result . "</b>";
+	}	
+}
+
+$kanny = new Student( );
+
+echo $kanny->result( 40 );
